a75d24d565d90f745ee781518154ae54ebeb0641
Dilshan841 <rathnayakehemantha427@gmail.com>
2025-04-17 10:36:59 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,19 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by DILSHAN ASHINSA*
+*_Pair Code Connected by DILSHAN MD*
 *_Made With 🚬🗿_*
 ______________________________________
 ╔════◇
-║ *『 AMAZING YOU'VE CHOSEN DILSHAN MD 』*
+║ *『 AMAZING YOU'VE CHOSEN ZENITSU CRASH V2 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Owner:🧑‍💻 | Dilshan Ashinsa | 🧑‍💻
-║❒ *Repo:*
-║❒ *WaGroup:*
-║❒ *WaChannel:*
+║❒ *Ytube:* _https://www.youtube.com/@BTSMODZ
+║❒ *Owner:* https://wa.me/94772194789
+║❒ *Repo:* _https://github.com/Fearless-tech1_
+║❒ *WaGroup:* _https://chat.whatsapp.com/C3GFThC0tIpGaJY9DFUeCK
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VahusSh0QeaoFzHJCk2x
+║❒ *Plugins:* _https://github.com/Fearless-tech1 
 ╚════════════════════════╝
 _____________________________________
 
